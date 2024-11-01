1d72c36a6a3c0554946153c745780f4416194dd5
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-11-01 19:02:15 +0900

    build before push

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next"
 import { Suspense } from "react"
-import { API_URL } from "../../../(home)/page"
 import MovieInfo, { getMovie } from "../../../../components/movie-info"
 import MovieVideos from "../../../../components/movie-videos"
 
